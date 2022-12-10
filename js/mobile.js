3aa274d91c13bcb8b3b90c08662abfccd9cd6926
dxdddddd <1731128703@qq.com>
2022-12-10 13:28:14 +0800

    提交文件

---
+++ js/mobile.js
@@ -0,0 +1,19 @@
+
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+      var url=window.location.href;
+      // 实现www跳转m
+      var url=url.replace("http://www.", "http://m.");
+      // 实现www以及子域名跳转m
+      // var url = url.replace(url.substr(0,url.indexOf('.')), 'http://m');
+      window.location.href=url;
+    }
